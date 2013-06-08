38803f7ed80f8a876a04ca6b55a73d29a349f016
ryan <836045106@qq.com>
2013-06-08 14:17:11 +0800

    ryan_添加注释
    Signed-off-by: ryan <836045106@qq.com>

---
+++ source/src/cn/eoe/app/RyanReading.java
@@ -0,0 +1,54 @@
+package cn.eoe.app;
+
+public class RyanReading {
+
+	/*
+	 * src目录的子目录 src <br>
+	 * 
+	 * ├ cn.eoe.app --存放程序全局性类的包<br>
+	 * 
+	 * ├ cn.eoe.app.adapter --存放适配器的实现类的包 <br>
+	 * 
+	 * ├ cn.eoe.app.adapter.base --存放适配器基类的包<br>
+	 * 
+	 * ├ cn.eoe.app.biz --存放ＤＡＯ类的包<br>
+	 * 
+	 * ├ cn.eoe.app.config －－存放常量，配置和api接口等类的包<br>
+	 * 
+	 * ├ cn.eoe.app.db --关于sqlite操作相关的类的包<br>
+	 * 
+	 * ├ cn.eoe.app.db.biz --详细的增删改查类的包，暂时仅有一个类<br>
+	 * 
+	 * ├ cn.eoe.app.entity --实体类包<br>
+	 * 
+	 * ├ cn.eoe.app.entity.base --实体类基类包<br>
+	 * 
+	 * ├ cn.eoe.app.https --网络访问相关类的包<br>
+	 * 
+	 * ├ cn.eoe.app.indicator --导航相关的类包<br>
+	 * 
+	 * ├ cn.eoe.app.slidingmenu --滑动菜单相关类包<br>
+	 * 
+	 * ├ cn.eoe.app.ui --界面相关的包，activity的类<br>
+	 * 
+	 * ├ cn.eoe.app.ui.base --activity相关的基类包<br>
+	 * 
+	 * ├ cn.eoe.app.utils --工具类包<br>
+	 * 
+	 * ├ cn.eoe.app.view --Fragment相关类的包<br>
+	 * 
+	 * ├ cn.eoe.app.widget --自定义view组件包<br>
+	 */
+
+	/*
+	 * libs
+	 * 
+	 * 
+	 * |- android-support-v4.jar --v4兼容包<br>
+	 * 
+	 * |jackson-all-1.9.2.jar --解析json的包<br>
+	 * 
+	 * |umeng_sdk.jar --友盟的sdk<br>
+	 */
+
+}
